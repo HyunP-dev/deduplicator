d09ecda8b571f5bfe7b9417cc5fee10b889bf0ee
Hyun Park <hyunp.dev@gmail.com>
2026-03-31 14:18:47 +0900

    Initial upload

---
+++ deduplicator/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using deduplicator.Views;
+using deduplicator.ViewModels;
+
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace deduplicator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        { 
+
+        }
+    }
+
+}
---
+++ deduplicator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace deduplicator.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string _inputText;
+
+        [ObservableProperty]
+        public string _resultText;
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public void Dedumplicate()
+        {
+            ResultText = String.Join("\n",
+                InputText.Split("\n")
+                .Select(s => s.Trim())
+                .Where(s => !s.IsWhiteSpace())
+                .Distinct()
+                .ToArray());
+        }
+
+        protected override void OnPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+
+            if (args.PropertyName == nameof(InputText))
+            {
+                Dedumplicate();
+            }
+        }
+    }
+}
